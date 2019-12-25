10d1422334fb7490376cf30e29704048b89f36e3
Will-Z <1025203332@qq.com>
2019-12-25 23:58:22 +0800

    msort

---
+++ src/algorithm/sort/MergeSort.java
@@ -0,0 +1,46 @@
+package algorithm.sort;
+
+/**
+ * @author will
+ * @date 2019/12/24
+ */
+public class MergeSort {
+    void mSort(int s, int t, int[] a, int[] r) {
+        if (s == t) {
+            return;
+        }
+        int m = s + t >> 1;
+        mSort(s, m, a, r);
+        mSort(m + 1, t, a, r);
+        int i = s;
+        int k = s;
+        int j = m + 1;
+        while (i <= m && j <= t) {
+            if (a[i] <= a[j]) {
+                r[k] = a[i]; i++; k++;
+            } else {
+                r[k] = a[j]; j++; k++;
+            }
+        }
+        while (i <= m) {
+            r[k] = a[i]; i++; k++;
+        }
+        while (j <= t) {
+            r[k] = a[j]; j++; k++;
+        }
+        for (int l = s; l <= t; l++) {
+            a[l] = r[l];
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = new int[] {43, 2, 3, 10, 1, 884, 6, 9, 48, 84};
+        int[] r = new int[10];
+        new MergeSort().mSort(0, 9, a, r);
+
+        for (int item : r) {
+            System.out.print(item + " ");
+        }
+
+    }
+}
